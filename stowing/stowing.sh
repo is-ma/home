0817489bfe5d89a998a288cde47bf601560fa6f1
Rich <14076090+is-ma@users.noreply.github.com>
2026-05-26 15:14:48 -0600

    home: stowing.sh + kdco scripts

---
+++ stowing/stowing.sh
@@ -0,0 +1,26 @@
+#################################################################
+#                          CONFIG FILES                         #
+#################################################################
+
+# espanso
+# mkdir -p ~/.config/espanso/match/
+# stow -t ~/.config/espanso/match/ espanso/
+
+# # helix
+# mkdir -p ~/.config/helix/runtime/themes/
+# stow -t ~/.config/helix/ helix_conf/
+# stow -t ~/.config/helix/runtime/themes/ helix_themes/
+
+# opencode
+mkdir -p ~/.config/opencode/
+stow -t ~/.config/opencode/ opencode/
+
+# dunst
+# mkdir -p ~/.config/dunst/
+# stow -t ~/.config/dunst/ dunst/
+
+#################################################################
+#                         CUSTOM SCRIPTS                        #
+#################################################################
+# mkdir -p ~/.local/bin/
+# stow -t ~/.local/bin/ custom_scripts/
