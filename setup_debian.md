f75b534de7341285e56a5a9e369b7970ea996eb0
Rich <14076090+is-ma@users.noreply.github.com>
2026-05-26 17:45:52 -0600

    i3/i3status/rofi: from dotfiles to home

--- setup_debian.md
+++ setup_debian.md
@@ -3,3 +3,11 @@
 ## alacritty
 sudo apt install -y alacritty
 
+## i3
+sudo apt install -y x11-xserver-utils  # xrand
+sudo apt install -y i3 i3status i3lock rofi xinit
+sudo apt install -y libnotify-bin dunst
+
+## browser
+sudo apt install -y firefox-esr
+
--- stowing/stowing.sh
+++ stowing/stowing.sh
@@ -6,22 +6,35 @@
 mkdir -p ~/.config/alacritty/
 stow -t ~/.config/alacritty/ alacritty/
 
+# dunst
+mkdir -p ~/.config/dunst/scripts/
+stow -t ~/.config/dunst/ dunst/
+
 # espanso
 mkdir -p ~/.config/espanso/match/
 stow -t ~/.config/espanso/match/ espanso/
 
-# # helix
+# helix
 mkdir -p ~/.config/helix/runtime/themes/
 stow -t ~/.config/helix/ helix_conf/
 stow -t ~/.config/helix/runtime/themes/ helix_themes/
+
+# i3
+mkdir -p ~/.config/i3/
+stow -t ~/.config/i3/ i3/
+
+# i3status
+mkdir -p ~/.config/i3status/
+stow -t ~/.config/i3status/ i3status/
 
 # opencode
 mkdir -p ~/.config/opencode/
 stow -t ~/.config/opencode/ opencode/
 
-# dunst
-mkdir -p ~/.config/dunst/scripts/
-stow -t ~/.config/dunst/ dunst/
+# rofi
+mkdir -p ~/.config/rofi/
+stow -t ~/.config/rofi/ rofi/
+
 
 #################################################################
 #                         CUSTOM SCRIPTS                        #
